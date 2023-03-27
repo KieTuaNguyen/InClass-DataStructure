d04db02b6d00fa442908bc6ceb91b193772a71e3
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-03-27 11:50:41 +0700

    Adding the elements into Stack_Assignment1

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,53 +1,26 @@
 import java.util.*;
 
 public class Main {
-    // Pushing elements into the stack
-    public static void push(Stack<Integer> stack) {
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-        }
-    }
-
-    // Popping elements from the stack
-    public static void pop(Stack<Integer> stack) {
-        System.out.println("Pop: ");
-
-        for (int i = 0; i < 5; i++) {
-            Integer y = (Integer) stack.pop();
-            System.out.println(y);
-        }
-    }
-
-    // Displaying the top element of the stack
-    public static void peek(Stack<Integer> stack) {
-        System.out.println("The top element in Stack:");
-        System.out.println(stack.peek());
-    }
-
-    // Searching an element in the stack
-    public static void search(Stack<Integer> stack, int element) {
-        Integer pos = (Integer) stack.search(element);
-
-        if (pos == -1)
-            System.out.println("Element not found");
-        else
-            System.out.println("Element is found at position " + pos);
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello Kiet!");
-        Stack<Integer> stack = new Stack<Integer>();
+        // Default initialization
+        Stack defaultStack = new Stack();
 
-        push(stack);
-        pop(stack);
-        System.out.println("---");
-        push(stack);
-        peek(stack);
-        System.out.println("---");
-        search(stack, 2);
-        search(stack, 4);
-        search(stack, 6);
-        System.out.println("---");
+        // Initialization with initial capacity
+        Stack<String> stack = new Stack<String>();
 
+        // Pushing elements into the stack
+        defaultStack.push("Kiet");
+        defaultStack.push("Tuan");
+        defaultStack.push("Nguyen");
+        defaultStack.push("GDD210002");
+
+        stack.push("Assignment 1");
+        stack.push("Data Structure");
+        stack.push("Algorithm");
+
+        // Displaying the Stack
+        System.out.println("   Default Stack: " + defaultStack);
+        System.out.println("   Stack: " + stack);
     }
 }
