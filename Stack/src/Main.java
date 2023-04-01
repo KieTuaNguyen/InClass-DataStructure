c47f241cecb8129de63f227876e6f33291166e13
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-04-01 10:51:22 +0700

    Stack_Assignment1

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,36 +1,18 @@
 import java.util.*;
 
 public class Main {
+    public static int square(int x) {
+        return x * x;
+    }
+
+    public static int sumOfSquares(int x, int y) {
+        return square(x + y);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Kiet!");
-        // Initialization with initial capacity
-        Stack<String> stack = new Stack<String>();
-
-        // Pushing elements into the stack
-        stack.push("Kiet Tuan Nguyen");
-        stack.push("GDD210002");
-        stack.push("Assignment 1");
-        stack.push("Data Structure");
-        stack.push("Stack");
-
-        // Accessing the elements in Stack:
-        // Displaying the size of Stack
-        System.out.println("   Size of Stack: " + stack.size());
-        // Displaying the Stack before pop
-        System.out.println("   Stack before pop: " + stack);
-        // Displaying the top element of Stack
-        System.out.println("   Top element of Stack: " + stack.peek());
-        System.out.println(
-                "-----------------------------------------------------------------------------------------------");
-        // Removing the elements using pop() method
-        System.out.println("   Popped element: " + stack.pop());
-        // Displaying the size of Stack
-        System.out.println("   Size of Stack: " + stack.size());
-        // Displaying the Stack after pop
-        System.out.println("   Stack after pop: " + stack);
-        // Displaying the top element of Stack
-        System.out.println("   Top element of Stack: " + stack.peek());
-        System.out.println(
-                "-----------------------------------------------------------------------------------------------");
+        int x = 3;
+        int y = 4;
+        int sum = sumOfSquares(x, y);
+        System.out.println("The sum of squares of " + x + " and " + y + " is " + sum + ".");
     }
 }
