084ebfa04b06d9e4dbae15c608a6436016851d1b
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-04-02 23:57:39 +0700

    Polymorphism in ADT Implementation with OOP_Assignment1

--- OOPRoleInImplementingADT/src/Main.java
+++ OOPRoleInImplementingADT/src/Main.java
@@ -1,12 +1,21 @@
+import implementations.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        PriorityStack priorityStack = new PriorityStack(5);
-        priorityStack.push(3);
-        priorityStack.push(1);
-        priorityStack.push(2);
-        System.out.println(priorityStack.removeMin());
-
-        priorityStack.push(4);
-        System.out.println(priorityStack.removeMin());
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("The stack includes: " + stack.toString());
+        System.out.println("---");
+        System.out.println("Peek an element: " + stack.peek());
+        System.out.println("The stack includes: " + stack.toString());
+        System.out.println("---");
+        System.out.println("Pop an element: ");
+        stack.pop();
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("The stack includes: " + stack.toString());
+        System.out.println("---");
     }
 }
