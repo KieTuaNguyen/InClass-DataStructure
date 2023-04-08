30bf1af9d3f4668cea878ff7a4e8dccf83a9e6e2
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-04-08 13:56:10 +0700

    Adding methods_Assignment2

--- Assignment2/KMessage/src/Main.java
+++ Assignment2/KMessage/src/Main.java
@@ -2,19 +2,41 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("KMessage");
-        System.out.println("Enter message (End with //)");
-        try {
-            String[] message = Methods.EnterMessage();
-            double beginTime = System.currentTimeMillis();
-            Methods.Transfer(message);
-            Methods.Process();
-            Methods.Print();
-            double endTime = System.currentTimeMillis();
-            double time = endTime - beginTime;
-            System.out.println("Time: " + time + "ms");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Methods.Menu();
+        System.out.println("Enter your choice: ");
+        int choice = 0;
+        do {
+            try {
+                choice = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+                continue;
+            }
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter message (End with //)");
+                    try {
+                        String[] message = Methods.EnterMessage(new BufferedReader(new InputStreamReader(System.in)));
+                        double beginTime = System.currentTimeMillis();
+                        Methods.Transfer(message);
+                        Methods.Process();
+                        double endTime = System.currentTimeMillis();
+                        double time = endTime - beginTime;
+                        System.out.println(" |Time: " + time + "ms");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    Methods.Print();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        } while (choice != 3);
     }
 }
--- Assignment2/KMessage/src/Methods.java
+++ Assignment2/KMessage/src/Methods.java
@@ -8,20 +8,18 @@ public class Methods {
   public static Queue<String> queue = new Queue<>();
   public static Stack<String> stack = new Stack<>();
 
-  public static String[] EnterMessage() throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  public static String[] EnterMessage(BufferedReader reader) throws IOException {
     String[] message = reader.readLine().split("//");
     int i = 0;
     while (i < message.length) {
       if (message[i].length() <= 0) {
         throw new IllegalArgumentException("Warning: Empty message ignored.");
-      } else if (message[i].length() >= 255) {
-        throw new IllegalArgumentException("Warning: Message exceeds 255 characters and will be truncated.");
+      } else if (message[i].length() >= 250) {
+        throw new IllegalArgumentException("Warning: Message exceeds 250 characters and will be truncated.");
       } else {
         break;
       }
     }
-    reader.close();
     return message;
   }
 
@@ -52,5 +50,11 @@ public class Methods {
       System.out.println(destination + ". ");
       stack.pop();
     }
+  }
+
+  public static void Menu() {
+    System.out.println("1. Enter message");
+    System.out.println("2. Print message");
+    System.out.println("3. Exit");
   }
 }
